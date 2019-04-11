8057f278cc3a021fc553a8b0630f56fbd4e96912
Daniel Ichiyama <danielichiyama@gmail.com>
2019-04-11 10:27:11 -1000

    exercise 12

--- exercises.js
+++ exercises.js
@@ -14,14 +14,14 @@ Objects in Javascript can be compared to objects in real life. Objects has a col
     Console.log the object.
 */
  
-// var books = {
-//     title: 'The Obstacle is the Way',
-//     author: 'Ryan Holiday',
-//     category: 'Self Help',
-//     pages: 200
-// }
+var books = {
+    title: 'The Obstacle is the Way',
+    author: 'Ryan Holiday',
+    category: 'Self Help',
+    pages: 200
+}
   
-// console.log(books);
+console.log(books);
 
 /*
 2.  Declare a variable named `dog` and create the following properties (key-value pairs) to the object:
@@ -38,16 +38,16 @@ Objects in Javascript can be compared to objects in real life. Objects has a col
  "My dog `name` is `age` year old and likes to `speak` at strangers."
 */ 
 
-// var dog = {
-//     name: 'Hercules',
-//     age: 20,
-//     vegetarian: false,
-//     color: 'golden brown',
-//     speak: function (){
-//         return 'bark!';
-//     }
-// }
-// console.log('My dog ' + dog.name + ' is ' + dog.age + ' years old and likes to ' + dog.speak() + ' at strangers.');
+var dog = {
+    name: 'Hercules',
+    age: 20,
+    vegetarian: false,
+    color: 'golden brown',
+    speak: function (){
+        return 'bark!';
+    }
+}
+console.log('My dog ' + dog.name + ' is ' + dog.age + ' years old and likes to ' + dog.speak() + ' at strangers.');
 
 //An empty object
 
@@ -64,17 +64,17 @@ Objects in Javascript can be compared to objects in real life. Objects has a col
   Console.log the object.
 */
 
-// var kicks = {};
+var kicks = {};
 
-// kicks.brand = 'Aldo';
-// kicks.color= 'cornflower blue';
-// kicks.size = 9.5;
-// kicks.price = 80.00;
-// kicks.buy = function (){
-//     return 'Now I is a cool kid!';
-// }
+kicks.brand = 'Aldo';
+kicks.color= 'cornflower blue';
+kicks.size = 9.5;
+kicks.price = 80.00;
+kicks.buy = function (){
+    return 'Now I is a cool kid!';
+}
 
-// console.log(kicks.buy());
+console.log(kicks.buy());
 /* 
 
 4. Declare a variable named `plainBox` and set its value to be an EMPTY object.
@@ -131,15 +131,15 @@ console.log(stockCar);
    the value at `name`, and just the value at `age`.
  */
 
-// var plainPerson = {};
+var plainPerson = {};
 
-// function buildPerson (person, nameString, age){
-//     person.name = nameString;
-//     person.age = age;
-//     return person
-// }
+function buildPerson (person, nameString, age){
+    person.name = nameString;
+    person.age = age;
+    return person
+}
 
-// var completePerson = buildPerson (plainPerson,'Thomas Edison', 172);
+var completePerson = buildPerson (plainPerson,'Thomas Edison', 172);
 
 // console.log(completePerson);
 // console.log(completePerson.name);
@@ -169,72 +169,72 @@ console.log(stockCar);
             ...
  */
 
-//  var arrayOfObjects = [
-//     {
-//       id: 0,
-//       date: "Monday Jan 25 2015 2:01 PM",
-//       total: "279.38"
-//     },
-//     {
-//       id: 1,
-//       date: "Monday Jan 27 2015 11:31 AM",
-//       total: "79.80"
-//     },
-//     {
-//       id: 2,
-//       date: "Monday Feb 1 2015 7:56 AM",
-//       total: "15.62"
-//     },
-//     {
-//       id: 3,
-//       date: "Monday Feb 1 2015 9:43 AM",
-//       total: "19.83"
-//     },
-//     {
-//       id: 4,
-//       date: "Monday Feb 1 2015 11:08 PM",
-//       total: "56.69"
-//     },
-//     {
-//       id: 5,
-//       date: "Monday Feb 13 2015 10:22 AM",
-//       total: "137.92"
-//     },
-//     {
-//       id: 6,
-//       date: "Monday Feb 14 2015 6:54 PM",
-//       total: "938.65"
-//     },
-//     {
-//       id: 7,
-//       date: "Monday Feb 14 2015 7:17 PM",
-//       total: "43.77"
-//     },
-//     {
-//       id: 8,
-//       date: "Monday Feb 14 2015 7:18 PM",
-//       total: "28.54"
-//     },
-//     {
-//       id: 9,
-//       date: "Monday Feb 14 2015 7:18 PM",
-//       total: "194.33"
-//     }
-//   ];
+ var arrayOfObjects = [
+    {
+      id: 0,
+      date: "Monday Jan 25 2015 2:01 PM",
+      total: "279.38"
+    },
+    {
+      id: 1,
+      date: "Monday Jan 27 2015 11:31 AM",
+      total: "79.80"
+    },
+    {
+      id: 2,
+      date: "Monday Feb 1 2015 7:56 AM",
+      total: "15.62"
+    },
+    {
+      id: 3,
+      date: "Monday Feb 1 2015 9:43 AM",
+      total: "19.83"
+    },
+    {
+      id: 4,
+      date: "Monday Feb 1 2015 11:08 PM",
+      total: "56.69"
+    },
+    {
+      id: 5,
+      date: "Monday Feb 13 2015 10:22 AM",
+      total: "137.92"
+    },
+    {
+      id: 6,
+      date: "Monday Feb 14 2015 6:54 PM",
+      total: "938.65"
+    },
+    {
+      id: 7,
+      date: "Monday Feb 14 2015 7:17 PM",
+      total: "43.77"
+    },
+    {
+      id: 8,
+      date: "Monday Feb 14 2015 7:18 PM",
+      total: "28.54"
+    },
+    {
+      id: 9,
+      date: "Monday Feb 14 2015 7:18 PM",
+      total: "194.33"
+    }
+  ];
 
-//   function printOrders (orders){
-//     array = [];
-//     for (i=0;i<orders.length;i++){  
-//         array.push('====='+"\n");
-//         array.push('id: ' + orders[i].id+"\n");
-//         array.push('purchase date: ' + orders[i].date+"\n");
-//         array.push('purchase total: ' + orders[i].total+"\n");
-//         //   console.log('=====');
-//         //   console.log('id: ' + orders[i].id);
-//         //   console.log('purchase date: ' + orders[i].date);
-//         //   console.log('purchase total: ' + orders[i].total);
-//       }return array.join('');
-//   }
+  function printOrders (orders){
+    array = [];
+    for (i=0;i<orders.length;i++){  
+        array.push('====='+"\n");
+        array.push('id: ' + orders[i].id+"\n");
+        array.push('purchase date: ' + orders[i].date+"\n");
+        array.push('purchase total: ' + orders[i].total+"\n");
+        //   console.log('=====');
+        //   console.log('id: ' + orders[i].id);
+        //   console.log('purchase date: ' + orders[i].date);
+        //   console.log('purchase total: ' + orders[i].total);
+      }return array.join('');
+  }
 
 //   console.log(printOrders(arrayOfObjects));
 
@@ -366,9 +366,14 @@ console.log(isAutomaticTransmission);
       your results. Consider using `plainPerson` as your driver.
  */
 
+function addDriver (object, person){
+    object.driver = person;
+    return object;
+}
 
+var stockCarWithDriver = addDriver (stockCar, completePerson);
 
-
+console.log (stockCarWithDriver);
 /*
     #Final Boss
     The Dev League instructors want to ride your whip!
